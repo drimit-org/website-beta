c72da63d15f8f86a0b87b59757cd4215e558d700
Diana Martínez <diananerdoficial@gmail.com>
2025-12-06 00:06:47 -0600

    Fix content

--- src/app/join/route.ts
+++ src/app/join/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-	return NextResponse.redirect("https://drimit.io/whatsapp/join", { status: 302 });
+	return NextResponse.redirect("https://drimit.io/whatsapp-join", { status: 302 });
 }
