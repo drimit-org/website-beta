aefb9f787d9a069a9ed315057a985b581d98e79f
Diana Martínez <diananerdoficial@gmail.com>
2025-12-05 23:52:24 -0600

    Fix issues

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
 	/* config options here */
+	outputFileTracingRoot: __dirname,
 };
 
 export default nextConfig;
