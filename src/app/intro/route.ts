1bf8eb0826524426c0075fd2d06c666c3f9f64a9
Diana Martínez <diananerdoficial@gmail.com>
2025-12-06 00:00:03 -0600

    Fix content

--- src/app/intro/route.ts
+++ src/app/intro/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-	return NextResponse.redirect("https://drimit.io/https://www.instagram.com/drimit.io", { status: 302 });
+	return NextResponse.redirect("https://www.instagram.com/drimit.io", { status: 302 });
 }
